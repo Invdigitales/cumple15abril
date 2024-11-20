b9d2b40101eff1ecfebeb014009bee5175bfb90d
Invdigitales <97065304+Invdigitales@users.noreply.github.com>
2024-11-20 11:04:12 -0300

    uno

---
+++ main.js
@@ -0,0 +1,79 @@
+
+let diaEvento = new Date('Enero 18, 2025 12:30:00').getTime();
+let boton = document.querySelector("#btn")
+
+
+
+// diaEvento = new Date().getTime() + 1_000 * 20;
+
+let relojE = document.getElementById('reloj');
+
+const diaM = 86_400_000;
+const horaM = 3_600_000;
+const minutoM = 60_000;
+const segundoM = 1_000;
+const ejecutarReloj = setInterval(
+    () => {
+
+        let hoy = new Date().getTime();
+        let distancia = diaEvento - hoy;
+
+        let diaR = Math.trunc(distancia / diaM);
+
+        let horaR = Math.trunc((distancia % diaM) / horaM);
+
+        let minutoR = Math.trunc((distancia % horaM) / minutoM);
+
+        let segundoR = Math.trunc((distancia % minutoM) / segundoM);
+
+        if (distancia  < 1) {
+            clearInterval(ejecutarReloj);
+            diaEventoAhora();
+        }
+
+        pintarElementos(diaR, horaR, minutoR, segundoR);
+
+    }
+, 1000);
+
+let creDia = document.getElementById('cre-dia');
+let creHora = document.getElementById('cre-hora');
+let creMinuto = document.getElementById('cre-minuto');
+let creSegundo = document.getElementById('cre-segundo');
+function pintarElementos(dia, hora, minuto, segundo) {
+
+    segundo = (segundo < 10) ? '0' + segundo : segundo;
+
+    minuto = (minuto < 10) ? '0' + minuto : minuto;
+
+    hora = (hora < 10) ? '0' + hora : hora;
+
+    creDia.innerHTML = dia;
+    creHora.innerHTML = hora;
+    creMinuto.innerHTML = minuto;
+    creSegundo.innerHTML = segundo;
+}
+
+function diaEventoAhora () {
+    alert('Dia del Evento!!!');
+    // Todo el codigo cuando el evento llegue
+}
+
+
+
+
+
+
+  boton.addEventListener("click", () => {
+
+    Swal.fire({
+      title: '<strong>VER LA UBICACION<u></u></strong>',
+      icon: 'success',
+      html:
+        'INGRESÁ' +
+        '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3277.5779790917345!2d-58.20371780000001!3d-34.766222299999995!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a328b2e02ae495%3A0xb7d070800b6ae3a5!2sC.%2020%20%26%20Diag.%20Lisandro%20de%20la%20Torre%2C%20B1880%20Berazategui%2C%20Provincia%20de%20Buenos%20Aires!5e0!3m2!1ses-419!2sar!4v1731016407606!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>' +
+        'Podes ampliar el mapa',
+        
+        
+    })
+  });
